d900b28687ef5298dcd8042be15f62ef96a3c5cc
matruby <Matthew.Rubie@flemingcollege.ca>
2023-01-18 13:27:57 -0500

    Created Script Files
    Added code that prints lines from the Zen of Python

---
+++ script1.py
@@ -0,0 +1,7 @@
+
+# Lab 1
+# script1.py  
+
+print("Beautiful is better than ugly.")
+print("Explicit is better than implicit.")
+
---
+++ script2.py
@@ -0,0 +1,7 @@
+
+# Lab 1 
+# script2.py
+
+print("Simple is better than complex.")
+print("Complex is better than complicated.")
+
